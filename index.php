6667385afd41fdf0d496201a29dad202208726b0
dream-weaver <eye_tanvir@hotmail.com>
2015-08-20 21:01:36 +0600

    Add folder structure to simple_cms for the cms test project

---
+++ index.php
@@ -0,0 +1,175 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>IXONY | Nogor template</title>
+    <!-- Bootstrap -->
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="bootstrap/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+
+
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <div class="container header-top">
+      <div class="row">
+        <div class="col-xs-12 col-sm-6 col-md-5 col-lg-5 pull-left">
+          <div class="logo">
+            <img class="img-responsive" src="images/logo_1428087229.png" alt="LOGO" title="LOGO"/> 
+          </div><!--logo ends-->
+        </div><!--col ends-->
+        <div class="col-xs-12 col-sm-6 col-md-7 col-lg-7 pull-right">
+          <nav>
+            <ul id="uplink">
+              <li><a href="contact.html">CONTACT |</a></li>
+              <li><a href="#">ABOUT |</a></li>
+              <li><a href="#">CAREER</a></li>
+            </ul>
+          </nav>
+        </div><!--uplink ends-->
+      </div><!--row ends-->
+    </div><!--header-top-->
+
+   
+    <div class="container top-menu">
+      <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 main-menu">
+          <nav class="navbar navbar-default-custom">
+            <div class="container-fluid"style="padding-left: 0px; padding-right: 0px;">
+              <!-- Brand and toggle get grouped for better mobile display -->
+              <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                  <span class="sr-only">Toggle navigation</span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                </button>
+              </div>
+              <!-- Collect the nav links, forms, and other content for toggling -->
+              <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-nav-custom">
+                  <li><a href="#">HOME</a></li>
+                  <li><a href="#">HOME</a></li>
+                  <li><a href="#">HOME</a></li>
+                  <li><a href="#">HOME</a></li>
+                  <li><a href="#">HOME</a></li>
+                  
+                  
+                </ul>
+
+              </div><!-- /.navbar-collapse -->
+            </div><!-- /.container-fluid -->
+          </nav>        
+        </div><!--menu ends-->
+      </div><!--row ends-->
+    </div><!--top-menu ends-->
+
+    <div class="container content">
+      <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+        <h1>Title</h1>
+        <p>Description</p>
+        <div class="navbar-text">
+        <h6><small><strong>SHARE: &nbsp;</strong></small></h6>
+        <a href="#"><i class="fa fa-facebook-square fa-2x"></i></a>
+        <a href="#"><i class="fa fa-twitter fa-2x"></i></a>
+        <a href="#"><i class="fa fa-google-plus fa-2x"></i></a>
+        <a href="#"><i class="fa fa-linkedin-square fa-2x"></i></a>
+        </div>
+        </div>
+      </div>
+    </div>
+
+
+     
+  
+  
+  <div class="container footer">
+    <div class="row">
+      <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+        <p class="copyright">Copyright &copy; 2015 <a href="http://www.nogorsolutions.com/">NOGOR Solutions</a></p>
+      </div><!--col ends-->
+      <div class="col-xs-12 col-sm-6 col-md-9 col-lg-9">
+        <nav>
+            <ul id="bottom-link">
+              <li><a href="#">Terms & Condition |</a></li>
+              <li><a href="#">Privacy Statement |</a></li>
+              <li><a href="#">Employee Access |</a></li>
+              <li><a href="contact.html">Contact |</a></li>
+              <li><a href="#">Careers</a></li>
+            </ul><!--bottom-link ends-->
+          </nav>
+        </div><!--uplink ends-->
+      </div><!--row-->
+    </div><!--footer ends-->
+
+
+
+  
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+    
+    <script>
+      $('#myCarousel').carousel({
+        interval:4000
+      });
+    </script>
+    <script>
+    $(document).ready(function() {
+  $('#hCarousel').carousel({
+  interval: 10000
+  })
+    
+    $('#hCarousel').on('slid.bs.carousel', function() {
+      //alert("slid");
+  });
+    
+    
+});
+
+
+    </script>
+
+  </body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
